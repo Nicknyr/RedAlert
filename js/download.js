0256d88394e7948d94d99c632630527c767783dd
Nick Kinlen <nick_kinlen@hotmail.com>
2017-11-19 16:44:01 -0500

    Layed out all pages

---
+++ js/download.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $('#windows-pic').click(function(){
+      $('#windows').show();
+    })
+
+    $('#mac-pic').click(function(){
+      $('#osx').show();
+    })
+
+    $('#debian-pic').click(function(){
+      $('#debian').show();
+    })
+
+    $('#fedora-pic').click(function(){
+      $('#fedora').show();
+    })
+
+    $('#arch-pic').click(function(){
+      $('#arch').show();
+    })
+
+    $('#gentoo-pic').click(function(){
+      $('#gentoo').show();
+    })
+
+    $('#exherbo-pic').click(function(){
+      $('#exherbo').show();
+    })
+
+    $('#bsd-pic').click(function(){
+      $('#freebsd').show();
+    })
+
+});
